6be45ac4a62039a6ce1c46ea896eaebc33438e06
Fajraoui Ghofrane <ghofranfa016@gmail.com>
2024-11-01 06:24:02 +0100

    update1

--- src/main/java/tn/esprit/tpfoyer/entity/Etudiant.java
+++ src/main/java/tn/esprit/tpfoyer/entity/Etudiant.java
@@ -5,6 +5,8 @@ import jakarta.persistence.*;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Set;
 
@@ -30,6 +32,25 @@ public class Etudiant {
 
     @ManyToMany(mappedBy = "etudiants")
     Set<Reservation> reservations;
+
+
+    public Etudiant(long idEtudiant, String nomEtudiant, String prenomEtudiant, long cinEtudiant, String dateNaissanceStr) {
+        this.idEtudiant = idEtudiant;
+        this.nomEtudiant = nomEtudiant;
+        this.prenomEtudiant = prenomEtudiant;
+        this.cinEtudiant = cinEtudiant;
+        this.dateNaissance = convertStringToDate(dateNaissanceStr);
+    }
+
+    private Date convertStringToDate(String dateStr) {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        try {
+            return sdf.parse(dateStr);
+        } catch (ParseException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 
 }
 
