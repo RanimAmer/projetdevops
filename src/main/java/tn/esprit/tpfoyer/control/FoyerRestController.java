48c9b503794b8b55d3ed778a07d42b26ff3657f1
SHIL YOSRA <yosra.shil@esprit.tn>
2024-11-04 10:02:43 +0100

    last One

--- src/main/java/tn/esprit/tpfoyer/control/FoyerRestController.java
+++ src/main/java/tn/esprit/tpfoyer/control/FoyerRestController.java
@@ -10,6 +10,7 @@ import java.util.List;
 @RestController
 @AllArgsConstructor
 @RequestMapping("/foyer")
+@CrossOrigin(origins = "http://localhost:4200")
 public class FoyerRestController {
 
     IFoyerService foyerService;
